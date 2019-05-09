d30bbe30d7461a660e05382628b9e18a0f80e271
Kevin Donahue <nonnontrivial@gmail.com>
2019-05-09 16:25:03 -0400

    add tests
    add circleci config

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
+const { join } = require('path');
 const { exec } = require('child_process');
 const { promisify } = require('util');
 
-const execP = promisify(exec);
-
-afterEach(async () => {
-  // Cleanup output directory
-  try {
-    await fs.promises.access(`${process.cwd()}/output`, fs.constants.R_OK);
-    await execP(`rm -rf ${process.cwd()}/output`);
-  } catch (_) {}
-});
+const exec_ = promisify(exec);
 
 test('does not write to stderr when done executing', async () => {
-  const { stdout, stderr } = await execP('npm start');
+  const { stdout, stderr } = await exec_('npm start');
   expect(stdout).toBeTruthy();
   expect(stderr).toBeFalsy();
 });
+
+test('produces /output', async () => {
+  await exec_('npm start');
+  expect(async () => {
+    await fs.promises.access(join(process.cwd(), 'output'));
+  }).not.toThrow();
+});
+
+test.todo('removes /output');
