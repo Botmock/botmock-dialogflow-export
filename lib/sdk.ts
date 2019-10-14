2b760c30930900e24367352c13ca62760e66dcd6
Kevin Donahue <nonnontrivial@gmail.com>
2019-10-14 15:08:09 -0400

    use new client wrapper

--- lib/sdk.ts
+++ lib/sdk.ts
@@ -1,19 +1,37 @@
-// import botmock from "botmock-js";
+import Botmock from "@botmock-api/client";
 import { EventEmitter } from "events";
 
-interface Config {}
+interface Config {
+  token: string;
+  teamId: string;
+  projectId: string;
+  boardId: string;
+}
 
 type JSONResponse = {};
 
 export default class SDKWrapper extends EventEmitter {
+  private readonly client: any;
   constructor(config: Config) {
     super();
+    this.client = new Botmock({ token: config.token });
   }
   /**
    * Fetches Botmock project data using the botmock SDK
-   * @returns Promise<JSONResponse>
+   * @returns Promise<{ data: JSONResponse }>
    */
   public async fetch(): Promise<{ data: JSONResponse }> {
-    return { data: {} }
+    try {
+      const assets = await this.client.fetchAssets([
+        "board",
+        "intents",
+        "entities",
+        "variables"
+      ]);
+      return { data: null };
+    } catch (err) {
+      this.emit("error", err);
+      return { data: {} };
+    }
   }
 }
