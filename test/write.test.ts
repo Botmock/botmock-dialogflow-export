0f6168a59b5c7e8cfb532d0334801c79859fa61b
Kevin Donahue <nonnontrivial@gmail.com>
2019-10-16 14:42:24 -0400

    add test for file writer

--- test/write.test.ts
+++ test/write.test.ts
@@ -1,3 +1,23 @@
+import { mkdirp, readdir, remove } from "fs-extra";
+import { join } from "path";
 import { default as FileWriter } from "../lib/file";
 
-test.skip("writes files to output dir", async () => {});
+let instance: FileWriter;
+const outputDirectory = join(__dirname, "test_output");
+beforeEach(async () => {
+  await mkdirp(outputDirectory);
+  instance = new FileWriter({
+    outputDirectory,
+    projectData: { board: { board: { messages: [] } } }
+  });
+});
+
+afterEach(async () => {
+  await remove(outputDirectory);
+});
+
+test("writes files to output dir", async () => {
+  await instance.write();
+  const contents = await readdir(outputDirectory);
+  expect(contents).toEqual(["agent.json", "package.json"]);
+});
