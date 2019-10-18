e9afd827cc69d10d7f2dd41914ca0b2b030002d6
Kevin Donahue <nonnontrivial@gmail.com>
2019-10-18 10:11:37 -0400

    add third integration test

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import { default as SDKWrapper } from "../lib/sdk";
 import { default as FileWriter } from "../lib/file";
-// import { default as BoardBoss } from "../lib/board";
+import { default as BoardBoss } from "../lib/board";
 // import { default as TextTransformer } from "../lib/text";
 // import { default as PlatformProvider } from "../lib/providers";
 import { execSync } from "child_process";
 import { EOL, tmpdir } from "os";
+import { mockProjectData } from "./fixtures";
 
 describe("run", () => {
   test("outputs correct number of newlines", () => {
@@ -35,6 +36,17 @@ describe("interaction of sdk wrapper and file writer", () => {
 });
 
 describe("interaction of board boss and file writer", () => {
-  // beforeEach(() => {});
-  test.todo("");
+  let fileWriterInstance: FileWriter;
+  const outputDirectory = tmpdir();
+  const projectData = mockProjectData;
+  beforeEach(() => {
+    fileWriterInstance = new FileWriter({ outputDirectory, projectData });
+  });
+  test("board boss can consume file writer project data", () => {
+    const board = fileWriterInstance.projectData.board.board;
+    const boardStructureByMessages = fileWriterInstance.segmentizeBoardFromMessages();
+    expect(() => {
+      new BoardBoss({ projectData, board, boardStructureByMessages })
+    }).not.toThrow();
+  });
 });
